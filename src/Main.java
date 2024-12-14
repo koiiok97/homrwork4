1a9c7f8702da8af2907c835f8a135dd7a8d2a46d
capybara97 <bezdomnyi1997@gmail.com>
2024-12-14 11:14:43 +0300

    ...

--- src/Main.java
+++ src/Main.java
@@ -26,21 +26,23 @@ public class Main {
         }
 
         System.out.println("\nЗадание 4");
-        int age1 = 18;
+        int age1 = 25;
         if(age1 >= 2 && age1 <= 6){
             System.out.println("Если возраст человека равен "+ age1 +", то ему нужно ходить в детский сад");
         }else if(age1 >= 7 && age1 <= 17){
             System.out.println("Если возраст человека равен "+ age1 +", то ему нужно ходить в школу");
         }else if(age1 >= 18 && age1 <= 24){
             System.out.println("Если возраст человека равен "+ age1 +", то ему нужно ходить в университет");
+        }else if (age1 > 24){
+            System.out.println("Если возраст человека равен "+ age1 +", то ему нужно ходить на работу");
         }
 
         System.out.println("\nЗадание 5");
-        int age2 = 15;
-        boolean isAdult = false;
-        if(age2 <= 5){
+        int age2 = 5;
+        boolean isAdult = true;
+        if(age2 < 5){
             System.out.println("Если возраст ребенка равен "+ age2 +", то ему нельзя кататься на аттракционе");
-        }else if(age2 > 5 && age2 <= 14 && isAdult){
+        }else if(age2 >= 5 && age2 <= 14 && isAdult){
             System.out.println("Если возраст ребенка равен "+ age2 +", то ему можно кататься на аттракционе в сопровождении");
         }else if(age2 > 14){
             System.out.println("Если возраст ребенка равен "+ age2 +", то ему можно без сопровождения взрослого");
